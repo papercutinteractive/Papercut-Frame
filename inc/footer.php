b5f8867c7a407b297c35db9e9e53c0ff6ac2076b
Matt Turnure <matt.turnure@papercutinteractive.com>
2012-08-03 23:29:07 -0400

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,13 @@
+    <footer id="footer-main" class="footer-main">
+      <!-- Footer -->
+    </footer>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script src="/js/core.js"></script>
+    <script>
+      var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+      (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+      s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
+  </body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Aframe</title>
+    
+    <meta name="description" content="Aframework" />
+    <meta name="author" content="Papercut Interactive" />
+    <meta name="viewport" content="width=device-width" />
+
+    <link rel="stylesheet" href="/styles/main.css" />
+
+    <script src="js/libs/modernizr-2.5.3-respond-1.1.0.min.js"></script>
+  </head>
+  <body>
+    <header id="header-main" class="header-main">
+      <!-- Branding and navigation -->
+    </header>
