e3d0588450047b6823180ef3633de67f68241b4c
Matt Turnure <matt.turnure@papercutinteractive.com>
2012-08-12 17:10:00 -0400

    created template parser

---
+++ inc/header-wireframes.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Aframe</title>
+    
+    <meta name="description" content="Aframework" />
+    <meta name="author" content="Papercut Interactive" />
+    <meta name="viewport" content="width=device-width" />
+
+    <link rel="stylesheet" href="/styles/wireframes.css" />
+
+    <script src="/js/modernizr-dev.js"></script>
+  </head>
+  <body>
+    <header id="header-main" class="header-main">
+      <!-- Branding and navigation -->
+    </header>
---
+++ views/template-index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Project Templates</title>
+    
+    <meta name="description" content="Aframework" />
+    <meta name="author" content="Papercut Interactive" />
+    <meta name="viewport" content="width=device-width" />
+
+    <link rel="stylesheet" href="/styles/main.css" />
+
+    <script src="/js/modernizr-dev.js"></script>
+  </head>
+  <body>
+    <header id="header-main" class="header-main">
+      <!-- Branding and navigation -->
+    </header>
+
+    <section id="content-main" class="content-main">
+      <h1>Project Templates</h1>
+      <ol>
+        <?=$template_files?>
+      </ol>
+    </section><!-- /.content-main -->
+  </body>
+</html>
